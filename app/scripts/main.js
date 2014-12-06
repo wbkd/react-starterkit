2ad9bd82ee1b8828b80e7d54e2791b9ccde4c238
moklick <info@moritzklack.com>
2014-12-06 18:17:45 +0100

    use routes.js file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,6 @@
 var React = require('react');
 var Router = require('react-router');
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
-var NotFoundRoute = Router.NotFoundRoute;
-
-var App = require('./pages/app.jsx');
-var Home = require('./pages/home.jsx');
-var Info = require('./pages/info.jsx');
-var NotFound = require('./pages/notFound.jsx');
-
-var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="info" handler={Info} />
-    <DefaultRoute handler={Home} />
-    <NotFoundRoute handler={NotFound}/>
-  </Route>
-);
+var routes = require('./routes');
 
 Router.run(routes, function (Handler) {
   React.render(<Handler/>, document.body);
---
+++ app/scripts/routes.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+
+var App = require('./pages/app.jsx');
+var Home = require('./pages/home.jsx');
+var Info = require('./pages/info.jsx');
+var NotFound = require('./pages/notFound.jsx');
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="info" handler={Info} />
+    <DefaultRoute handler={Home} />
+    <NotFoundRoute handler={NotFound}/>
+  </Route>
+);
+
+module.exports = routes;
