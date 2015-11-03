371f530c5d32568123032a169aa2c2beca7efa3f
moklick <info@moritzklack.com>
2015-11-03 22:48:17 +0100

    update router history handling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import routes from './routes';
 
+
 ReactDOM.render(routes, document.getElementById('app'));
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
+import createHistory from 'history/lib/createHashHistory'
 
 import App from './pages/app.jsx';
 import Home from './pages/home.jsx';
 import Info from './pages/info.jsx';
 import NotFound from './pages/notFound.jsx';
 
-var routes = (
-  <Router>
-    <Route path='app' path='/' component={ App }>
+const historyOptions = {
+  queryKey : false
+};
+
+const routes = (
+  <Router history={createHistory(historyOptions)}>
+    <Route path='/' component={ App }>
+      <IndexRoute component={ Home }/>
       <Route path='info' component={ Info } />
       <Route path='home' component={ Home } />
-      <IndexRoute component={Home}/>
       <Route path='*' component={NotFound}/>
     </Route>
   </Router>
