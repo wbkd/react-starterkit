82bdf9f7c4a575df3e8834df9565e2fdd9ee096d
moklick <info@moritzklack.com>
2015-11-04 00:05:51 +0100

    update tests

--- app/scripts/__tests__/itemlist-test.js
+++ app/scripts/__tests__/itemlist-test.js
@@ -2,25 +2,25 @@
 
 jest.dontMock('../components/itemList.jsx');
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
-var ItemList = require('../components/itemList.jsx');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+let ItemList = require('../components/itemList.jsx');
 
 describe('item list', function() {
 
   it('displays three items with right text content', function() {
 
-    var testItems = ['foo', 'bar', 'wut'],
+    const testItems = ['foo', 'bar', 'wut'],
       itemList = TestUtils.renderIntoDocument(<div><ItemList items={ testItems } loading={ false } /></div>);
 
     expect(ReactDOM.findDOMNode(itemList).textContent).toEqual(testItems.join(''));
   });
 
   it('displays loading div', function() {
-    var itemList = TestUtils.renderIntoDocument(<div><ItemList items={[]} loading={ true } /></div>);
+    const itemList = TestUtils.renderIntoDocument(<div><ItemList items={[]} loading={ true } /></div>);
 
     expect(ReactDOM.findDOMNode(itemList).textContent).toEqual('Loading...');
   });
-
 });
